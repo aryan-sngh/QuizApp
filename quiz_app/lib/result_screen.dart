3f2031b936d7542696beddf5bc4eac0919abaa41
Aryan Singh <arya.singh8930@gmail.com>
2024-12-06 13:36:00 +0530

    added result screen

---
+++ quiz_app/lib/result_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
