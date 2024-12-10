dbac60e47b3b326e4af0684298a0e1dbeb054abd
Aryan Singh <arya.singh8930@gmail.com>
2024-12-10 12:59:52 +0530

    Update questions_summary.dart

--- quiz_app/lib/questions_summary.dart
+++ quiz_app/lib/questions_summary.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class QuestionsSummary extends StatelessWidget {
-  const QuestionsSummary(this.summaryData,{super.key});
+  const QuestionsSummary(this.summaryData, {super.key});
 
-  final List<Map<String,Object>> summaryData;
+  final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: summaryData.map(
+        (data) {
+          return Row(
+            children: [
+              Text(((data['question_index'] as int) + 1).toString()),
+            ],
+          );
+        },
+      ).toList(),
+    );
   }
 }
