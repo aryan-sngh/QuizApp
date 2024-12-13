75cc2ae2cedfc40cd5f7aa6ec98a9661bcd7224b
Aryan Singh <arya.singh8930@gmail.com>
2024-12-13 13:27:25 +0530

    modified result with question summary

--- quiz_app/lib/result_screen.dart
+++ quiz_app/lib/result_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/questions_summary.dart';
+import 'package:quiz_app/questions_summary/questions_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen(
